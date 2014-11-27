641293b30521211d9f23451c01a8cda0e48ad374
Kevin Moll <kevin.j.moll@gmail.com>
2014-11-27 12:03:44 -0500

    2291249 implementing Modal display

--- src/DisplayInterface.php
+++ src/DisplayInterface.php
@@ -46,4 +46,17 @@ interface DisplayInterface extends PluginInspectionInterface, ConfigurablePlugin
    *
    */
   public function selectionCompleted(array $entities);
+
+  /**
+   * Adds ajax capabilities to the entity browser form.
+   *
+   * This will be used in Plugins like Modal that require the Entity Browser
+   * form to be sumbitted with ajax.  All other plugins that don't require it
+   * can leave it blank.
+   *
+   * @param array $form
+   *   Form array containing the Entity Browser elements.
+   */
+  public function addAjax(array &$form);
+
 }
