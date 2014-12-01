e6632a371110bb684a825040caee6fa9b0afc416
Kevin Moll <kevin.j.moll@gmail.com>
2014-11-30 20:03:41 -0500

    2291249 removed commented out code

--- js/entity_browser.entity_reference.js
+++ js/entity_browser.entity_reference.js
@@ -19,7 +19,6 @@
    *   Array of selected entities.
    */
   Drupal.entityBrowserEntityReference.selectionCompleted = function(event, uuid, entities) {
-
     // Update value form element with new entity IDs.
     var entity_ids = $(this).parent().parent().find('input[type*=hidden]').val();
     if (entity_ids.length != 0) {
@@ -33,10 +32,6 @@
     // TODO - temporary solution. See: https://www.drupal.org/node/2366241 and
     // \Drupal\entity_browser\Plugin\Field\Widget\EntityReference.
     $(this).parent().parent().find('div.current-markup').html(entity_ids);
-
-    // Display "Select entities" link and destroy iFrame.
-//    $(this).parent().find('a[data-uuid*=' + uuid + ']').show();
-//    $(this).remove();
   }
 
 }(jQuery, Drupal, drupalSettings));
