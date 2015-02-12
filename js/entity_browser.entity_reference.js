1667e2723af9b57a679f60c5296d636c67e6e0a7
Janez Urevc <janez@janezurevc.name>
2015-02-12 15:05:58 +0100

    Issue #2400433 by slashrsm: Allow sorting of entities in entity reference widget.

--- js/entity_browser.entity_reference.js
+++ js/entity_browser.entity_reference.js
@@ -7,6 +7,18 @@
 (function ($, Drupal, drupalSettings) {
 
   "use strict";
+  /**
+   * Registers behaviours related to entity reference field widget.
+   */
+  Drupal.behaviors.entityBrowserEntityReference = {
+    attach: function (context) {
+      $(context).find('.field-widget-entity-browser-entity-reference').each(function () {
+        $(this).find('.entities-list').sortable({
+          stop: Drupal.entityBrowserEntityReference.entitiesReordered
+        });
+      });
+    }
+  };
 
   Drupal.entityBrowserEntityReference = {};
 
@@ -28,6 +40,24 @@
     entity_ids = entity_ids + $.map(entities, function(item) {return item[0]}).join(' ');
     $(this).parent().parent().find('input[type*=hidden]').val(entity_ids);
     $(this).parent().parent().find('input[type*=hidden]').trigger('entity_browser_value_updated');
-  }
+  };
+
+  /**
+   * Reacts on sorting of the entities.
+   *
+   * @param event
+   *   Event object.
+   * @param ui
+   *   Object with detailed information about the sort event.
+   */
+  Drupal.entityBrowserEntityReference.entitiesReordered = function(event, ui) {
+    var items = $(this).find('li');
+    var ids = [];
+    for (var i = 0; i < items.length; i++) {
+      ids[i] = $(items[i]).attr('data-entity-id');
+    }
+
+    $(this).parent().parent().find('input[type*=hidden]').val(ids.join(' '));
+  };
 
 }(jQuery, Drupal, drupalSettings));
