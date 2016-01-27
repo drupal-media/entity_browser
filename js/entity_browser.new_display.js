fb32f3e88f7da7dad7b17890d8ffc8e1556383b1
Nenad Nesic <nndnsc@gmail.com>
2016-01-27 10:03:42 +0100

    fix some drag&drop reordering

---
+++ js/entity_browser.new_display.js
@@ -0,0 +1,42 @@
+/**
+ * @file entity_browser.entity_reference.js
+ *
+ */
+(function ($, Drupal, drupalSettings) {
+
+  "use strict";
+
+  /**
+   * Registers behaviours related to selected entities.
+   */
+  Drupal.behaviors.entityBrowserNewDisplay = {
+    attach: function (context) {
+      $(context).find('.entity-browser-test-files-form').each(function () {
+        $(this).find('.selected-entities-list').sortable({
+          stop: Drupal.entityBrowserNewDisplay.entitiesReordered
+        });
+      });
+    }
+  };
+
+  Drupal.entityBrowserNewDisplay = {};
+
+  /**
+   * Reacts on sorting of the entities.
+   *
+   * @param event
+   *   Event object.
+   * @param ui
+   *   Object with detailed information about the sort event.
+   */
+  Drupal.entityBrowserNewDisplay.entitiesReordered = function(event, ui) {
+    var items = $(this).find('.selected-item-container');
+    var ids = [];
+    for (var i = 0; i < items.length; i++) {
+      ids[i] = $(items[i]).attr('data-entity-id');
+    }
+
+    $(this).parent().parent().find('input[type*=hidden]').val(ids.join(' '));
+  };
+
+}(jQuery, Drupal, drupalSettings));
