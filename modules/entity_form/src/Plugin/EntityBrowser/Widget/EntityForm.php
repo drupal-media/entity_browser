a3faf0b169c47b2c3e3837b651d5c04dd53e2372
slashrsm <slashrsm@744628.no-reply.drupal.org>
2016-02-26 20:28:09 +0530

    Issue #2674698 by slashrsm, Primsi: [Chase IEF] Rely on IEF for submit and save

--- modules/entity_form/src/Plugin/EntityBrowser/Widget/EntityForm.php
+++ modules/entity_form/src/Plugin/EntityBrowser/Widget/EntityForm.php
@@ -41,7 +41,6 @@ class EntityForm extends WidgetBase {
       'inline_entity_form' => [
         '#type' => 'inline_entity_form',
         '#op' => 'add',
-        '#handle_submit' => FALSE,
         '#entity_type' => $this->configuration['entity_type'],
         '#bundle' => $this->configuration['bundle'],
       ],
@@ -52,11 +51,6 @@ class EntityForm extends WidgetBase {
    * {@inheritdoc}
    */
   public function submit(array &$element, array &$form, FormStateInterface $form_state) {
-    // We handle submit on our own in order to take control over what's going on.
-    foreach ($element['inline_entity_form']['#ief_element_submit'] as $function) {
-      $function($element['inline_entity_form'], $form_state);
-    }
-
     $this->selectEntities([$element['inline_entity_form']['#entity']], $form_state);
   }
 
