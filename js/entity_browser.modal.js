a830430b9e5e17360713017eb3cdd8f5a18980b7
Jonathan Minder <jonathan.minder@ayalon.ch>
2016-03-10 15:23:51 +0100

    Entity Browser Responsive Modal Dialog

--- js/entity_browser.modal.js
+++ js/entity_browser.modal.js
@@ -37,4 +37,60 @@
       });
     }
   };
+
+  Drupal.behaviors.fluidModal = {
+    attach: function (context) {
+
+      // on window resize run function
+      $(window).resize(function (context) {
+        fluidDialog();
+      });
+
+      // catch dialog if opened within a viewport smaller than the dialog width
+      // run function on all dialog opens
+      $(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+        fluidDialog();
+      });
+    }
+  };
+
+  var fluidDialog = function fluidDialog() {
+    var $visible = $(".ui-dialog:visible");
+    // each open dialog
+    $visible.each(function () {
+      var $this = $(this);
+      var dialog = $this.find(".ui-dialog-content").data("ui-dialog");
+      // if fluid option == true
+      if (dialog.options.fluid) {
+        var wWidth = $(window).width();
+        // check window width against dialog width
+        if (wWidth < (dialog.options.maxWidth + 50)) {
+          //if there is a maxWidth, don't allow a bigger size
+          dialog.option("width", dialog.options.maxWidth);
+        } else {
+          // if no maxWidth is defined, make it responsive
+          dialog.option("width", '92%');
+        }
+
+        var vHeight = $(window).height();
+        // check window width against dialog width
+        if (vHeight < (dialog.options.maxHeight + 50)) {
+          //if there is a maxHeight, don't allow a bigger size
+          dialog.option("height", dialog.options.maxHeight);
+        } else {
+          // if no maxHeight is defined, make it responsive
+          dialog.option("height", vHeight-100);
+
+          // Because there is no iframe height 100% in HTML 5, we have to set the height of the iframe as well
+          var contentHeight = $this.find('.ui-dialog-content').height() - 20;
+          $this.find("iframe").css("height", contentHeight);
+        }
+
+        //reposition dialog
+        dialog.option("position", dialog.options.position);
+      }
+    });
+  };
+
+
 }(jQuery, Drupal, drupalSettings));
