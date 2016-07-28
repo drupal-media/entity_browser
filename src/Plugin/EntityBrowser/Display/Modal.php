da8d51298a4846245a5f9715c797cbb1988f1273
slashrsm <slashrsm@744628.no-reply.drupal.org>
2016-07-28 15:49:57 +0200

    Issue #2774661 by slashrsm, Berdir: Consistently use underscores for iframe names

--- src/Plugin/EntityBrowser/Display/Modal.php
+++ src/Plugin/EntityBrowser/Display/Modal.php
@@ -207,7 +207,7 @@ class Modal extends DisplayBase implements DisplayRouterInterface {
         'height' => $this->configuration['height'] - 90,
         'frameborder' => 0,
         'style' => 'padding:0',
-        'name' => 'entity_browser_iframe_' . Html::cleanCssIdentifier($element_name),
+        'name' => 'entity_browser_iframe_' . $element_name,
       ],
     ];
     $html = drupal_render($content);
