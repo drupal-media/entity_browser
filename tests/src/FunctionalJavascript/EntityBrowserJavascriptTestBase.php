31f34ff121c79a62decbfd06142d07f9be0f461e
git <git@1275178.no-reply.drupal.org>
2016-08-08 12:41:46 +0200

    Issue #2765789 by slashrsm, mtodor, Denchev, chr.fritsch: Create basic functional havascript test.

---
+++ tests/src/FunctionalJavascript/EntityBrowserJavascriptTestBase.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Drupal\Tests\entity_browser\FunctionalJavascript;
+
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\file\Entity\File;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+
+/**
+ * Base class for Entity browser Javascript functional tests.
+ *
+ * @package Drupal\Tests\entity_browser\FunctionalJavascript
+ */
+abstract class EntityBrowserJavascriptTestBase extends JavascriptTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'entity_browser_test',
+    'ctools',
+    'views',
+    'block',
+    'node',
+    'file',
+    'image',
+    'field_ui',
+    'views_ui',
+    'system',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
+
+    FieldStorageConfig::create([
+      'field_name' => 'field_reference',
+      'type' => 'entity_reference',
+      'entity_type' => 'node',
+      'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
+      'settings' => [
+        'target_type' => 'file',
+      ],
+    ])->save();
+
+    FieldConfig::create([
+      'field_name' => 'field_reference',
+      'entity_type' => 'node',
+      'bundle' => 'article',
+      'label' => 'Reference',
+      'settings' => [],
+    ])->save();
+
+    /** @var \Drupal\Core\Entity\Display\EntityFormDisplayInterface $form_display */
+    $form_display = $this->container->get('entity_type.manager')
+      ->getStorage('entity_form_display')
+      ->load('node.article.default');
+
+    $form_display->setComponent('field_reference', [
+      'type' => 'entity_browser_entity_reference',
+      'settings' => [
+        'entity_browser' => 'test_entity_browser_file',
+        'field_widget_display' => 'label',
+        'open' => TRUE,
+      ],
+    ])->save();
+
+    $account = $this->drupalCreateUser([
+      'access test_entity_browser_file entity browser pages',
+      'create article content',
+      'access content',
+    ]);
+    $this->drupalLogin($account);
+  }
+
+  /**
+   * Return an entity browser if it exists or creates a new one.
+   *
+   * @param string $browser_name
+   *   The entity browser name.
+   * @param string $display_id
+   *   The display plugin id.
+   * @param string $widget_selector_id
+   *   The widget selector id.
+   * @param string $selection_display_id
+   *   The selection display id.
+   * @param array $display_configuration
+   *   The display plugin configuration.
+   * @param array $widget_selector_configuration
+   *   The widget selector configuration.
+   * @param array $selection_display_configuration
+   *   The selection display configuration.
+   *
+   * @return \Drupal\entity_browser\EntityBrowserInterface
+   *   Returns an Entity Browser.
+   */
+  protected function getEntityBrowser($browser_name, $display_id, $widget_selector_id, $selection_display_id, $display_configuration = [], $widget_selector_configuration = [], $selection_display_configuration = []) {
+    /** @var \Drupal\Core\Entity\EntityStorageInterface $storage */
+    $storage = $this->container->get('entity_type.manager')
+      ->getStorage('entity_browser');
+
+    /** @var \Drupal\entity_browser\EntityBrowserInterface $browser */
+    $browser = $storage->load($browser_name) ?: $storage->create(['name' => $browser_name]);
+
+    $browser->setDisplay($display_id);
+    if ($display_configuration) {
+      $browser->getDisplay()->setConfiguration($display_configuration);
+    }
+
+    $browser->setWidgetSelector($widget_selector_id);
+    if ($widget_selector_configuration) {
+      $browser->getSelectionDisplay()
+        ->setConfiguration($widget_selector_configuration);
+    }
+
+    $browser->setSelectionDisplay($selection_display_id);
+    if ($selection_display_configuration) {
+      $browser->getSelectionDisplay()
+        ->setConfiguration($selection_display_configuration);
+    }
+    $browser->save();
+
+    // Clear caches after new browser is saved to remove old cached states.
+    drupal_flush_all_caches();
+
+    return $browser;
+  }
+
+  /**
+   * Creates an image.
+   *
+   * @param string $name
+   *   The name of the image.
+   *
+   * @return \Drupal\file\FileInterface
+   *   Returns an image.
+   */
+  protected function createFile($name) {
+    file_put_contents('public://' . $name . '.jpg', $this->randomMachineName());
+
+    $image = File::create([
+      'filename' => $name . '.jpg',
+      'uri' => 'public://' . $name . '.jpg',
+    ]);
+    $image->save();
+
+    return $image;
+  }
+
+  /**
+   * Waits for jQuery to become ready and animations to complete.
+   */
+  protected function waitForAjaxToFinish() {
+    $this->assertSession()->assertWaitOnAjaxRequest();
+  }
+
+  /**
+   * Waits and asserts that a given element is visible.
+   *
+   * @param string $selector
+   *   The CSS selector.
+   * @param int $timeout
+   *   (Optional) Timeout in milliseconds, defaults to 1000.
+   * @param string $message
+   *   (Optional) Message to pass to assertJsCondition().
+   */
+  protected function waitUntilVisible($selector, $timeout = 1000, $message = '') {
+    $condition = "jQuery('" . $selector . ":visible').length > 0";
+    $this->assertJsCondition($condition, $timeout, $message);
+  }
+
+}
---
+++ tests/src/FunctionalJavascript/EntityBrowserTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Drupal\Tests\entity_browser\FunctionalJavascript;
+
+/**
+ * Tests the entity_browser.
+ *
+ * @group entity_browser
+ */
+class EntityBrowserTest extends EntityBrowserJavascriptTestBase {
+
+  /**
+   * Tests single widget selector.
+   */
+  public function testSingleWidgetSelector() {
+
+    // Sets the single widget selector.
+    /** @var \Drupal\entity_browser\EntityBrowserInterface $browser */
+    $browser = $this->container->get('entity_type.manager')
+      ->getStorage('entity_browser')
+      ->load('test_entity_browser_file');
+
+    $this->assertEquals($browser->getWidgetSelector()->getPluginId(), 'single', 'Widget selector is set to single.');
+
+    // Create a file.
+    $image = $this->createFile('llama');
+
+    $this->drupalGet('node/add/article');
+
+    $this->assertSession()->linkExists('Select entities');
+    $this->getSession()->getPage()->clickLink('Select entities');
+
+    $this->getSession()->switchToIFrame('entity_browser_iframe_test_entity_browser_file');
+
+    $this->getSession()->getPage()->checkField('entity_browser_select[file:' . $image->id() . ']');
+    $this->getSession()->getPage()->pressButton('Select entities');
+
+    $this->assertSession()->pageTextContains('llama.jpg');
+  }
+
+  /**
+   * Tests tabs widget selector.
+   */
+  public function testTabsWidgetSelector() {
+
+    // Sets the tabs widget selector.
+    /** @var \Drupal\entity_browser\EntityBrowserInterface $browser */
+    $browser = $this->container->get('entity_type.manager')
+      ->getStorage('entity_browser')
+      ->load('test_entity_browser_file')
+      ->setWidgetSelector('tabs');
+    $browser->save();
+
+    $this->assertEquals($browser->getWidgetSelector()->getPluginId(), 'tabs', 'Widget selector is set to tabs.');
+
+    // Create a file.
+    $image = $this->createFile('llama');
+
+    // Create a second file.
+    $image2 = $this->createFile('llama2');
+
+    $this->drupalGet('node/add/article');
+
+    $this->assertSession()->linkExists('Select entities');
+    $this->getSession()->getPage()->clickLink('Select entities');
+
+    $this->getSession()->switchToIFrame('entity_browser_iframe_test_entity_browser_file');
+
+    $this->assertSession()->linkExists('view');
+    $this->assertSession()->linkExists('upload');
+
+    $this->assertEquals('is-active active', $this->getSession()->getPage()->findLink('view')->getAttribute('class'));
+
+    $this->getSession()->getPage()->checkField('entity_browser_select[file:' . $image->id() . ']');
+    $this->getSession()->getPage()->pressButton('Select entities');
+    $this->getSession()->switchToIFrame();
+
+    $this->waitForAjaxToFinish();
+
+    $this->assertSession()->pageTextContains('llama.jpg');
+
+    $this->getSession()->getPage()->clickLink('Select entities');
+    $this->getSession()->switchToIFrame('entity_browser_iframe_test_entity_browser_file');
+    $this->getSession()->getPage()->clickLink('upload');
+
+    // This is producing an error. Still investigating
+    // InvalidStateError: DOM Exception 11: An attempt was made to use an object
+    // that is not, or is no longer, usable.
+    //$edit = [
+    //  'files[upload][]' => $this->container->get('file_system')->realpath($image2->getFileUri()),
+    //];
+    // $this->drupalPostForm(NULL, $edit, 'Select files');.
+  }
+
+  /**
+   * Tests dropdown widget selector.
+   */
+  public function testDropdownWidgetSelector() {
+
+    // Sets the dropdown widget selector.
+    /** @var \Drupal\entity_browser\EntityBrowserInterface $browser */
+    $browser = $this->container->get('entity_type.manager')
+      ->getStorage('entity_browser')
+      ->load('test_entity_browser_file')
+      ->setWidgetSelector('drop_down');
+    $browser->save();
+
+    $this->assertEquals($browser->getWidgetSelector()->getPluginId(), 'drop_down', 'Widget selector is set to dropdown.');
+
+    // Create a file.
+    $image = $this->createFile('llama');
+
+    $this->drupalGet('node/add/article');
+
+    $this->assertSession()->linkExists('Select entities');
+    $this->getSession()->getPage()->clickLink('Select entities');
+
+    $this->getSession()->switchToIFrame('entity_browser_iframe_test_entity_browser_file');
+
+    $this->assertSession()->selectExists('widget');
+    // Selects the view widget.
+    $this->getSession()->getPage()->selectFieldOption('widget', '774798f1-5ec5-4b63-84bd-124cd51ec07d');
+
+    $this->getSession()->getPage()->checkField('entity_browser_select[file:' . $image->id() . ']');
+    $this->getSession()->getPage()->pressButton('Select entities');
+
+    $this->assertSession()->pageTextContains('llama.jpg');
+    // Return from iframe to the node add page.
+    $this->getSession()->switchToIFrame();
+    $this->waitForAjaxToFinish();
+
+    $this->getSession()->getPage()->clickLink('Select entities');
+
+    $this->getSession()->switchToIFrame('entity_browser_iframe_test_entity_browser_file');
+
+    // Causes a fatal.
+    // Selects the upload widget.
+    // $this->getSession()->getPage()->selectFieldOption('widget', '2dc1ab07-2f8f-42c9-aab7-7eef7f8b7d87');.
+  }
+
+  /**
+   * Tests wievs selection display.
+   */
+  public function testViewsSelectionDisplayWidget() {
+
+    // Sets the dropdown widget selector.
+    /** @var \Drupal\entity_browser\EntityBrowserInterface $browser */
+    $browser = $this->container->get('entity_type.manager')
+      ->getStorage('entity_browser')
+      ->load('test_entity_browser_file')
+      ->setSelectionDisplay('view');
+    $browser->save();
+
+    $this->assertEquals($browser->getSelectionDisplay()->getPluginId(), 'view', 'Selection display is set to view.');
+
+  }
+
+}
