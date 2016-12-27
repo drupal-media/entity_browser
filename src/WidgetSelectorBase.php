d50191f950778df2ec90c59e31f3c9338139e9d7
fago <fago@16747.no-reply.drupal.org>
2016-12-27 10:10:53 +0100

    Issue #2836341 by fago: Fix Notice when the module is installed during config sync.

--- src/WidgetSelectorBase.php
+++ src/WidgetSelectorBase.php
@@ -22,9 +22,9 @@ abstract class WidgetSelectorBase extends PluginBase implements WidgetSelectorIn
   /**
    * Available widgets.
    *
-   * @var array()
+   * @var array
    */
-  protected $widgets_options;
+  protected $widgets_ids;
 
   /**
    * ID of the default widget.
@@ -38,7 +38,7 @@ abstract class WidgetSelectorBase extends PluginBase implements WidgetSelectorIn
    */
   public function __construct($configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->widget_ids = $this->configuration['widget_ids'];
+    $this->widget_ids = isset($this->configuration['widget_ids']) ? $this->configuration['widget_ids'] : [];
   }
 
   /**
